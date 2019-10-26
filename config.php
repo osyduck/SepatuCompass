6d9f33f9f97479ad21d950e3dcd74d5ef9b85af1
Janu Yoga <onlypaymenthere@gmail.com>
2019-10-26 14:30:50 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $URL_API    = "https://lupabapak.com/sepatu/api.php";
-$BOT_TOKEN  = "734569645:AAF6nwxR0C7u1Zssfeef3PyCqAOt3wMxwk"
+$BOT_TOKEN  = "734569645:AAF6nwxR0C7u1Zssfeef3PyCqAOt3wMxwk";
 $CHAT_ID    = "4212002460";
 
 ?>
